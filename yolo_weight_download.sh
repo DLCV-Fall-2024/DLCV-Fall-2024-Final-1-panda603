5368e4a9169ce96fcc722d681bbe2c8770e62dd0
pryang0601 <87400358+pryang0601@users.noreply.github.com>
2024-12-26 16:29:41 +0800

    yolo weight upload

--- yolo_weight_download.sh
+++
@@ -1,4 +0,0 @@
-# bdd_best.pt
-wget -O bdd_best.pt https://www.dropbox.com/scl/fi/szo2aq07juepp9cp9xxau/bdd_best.pt?rlkey=nlqza7uzrjg9io03wmbonqkel&st=9m0tycmy&dl=1
-# cone_best.pt
-wget -O cone_best.pt https://www.dropbox.com/scl/fi/hi4f6yop9ipj5d41d5ulz/cone_best.pt?rlkey=1n0qk7nc4roh0jyi21ahmdgez&st=ho8ivefc&dl=1
